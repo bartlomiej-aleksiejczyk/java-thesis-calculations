d2c89d13ae024fc28db030383afea186a59776f0
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-15 13:24:37 +0200

    Refactor saveObjectToCsv for better object handling

--- src/main/java/org/example/savecsv/ResultStructure.java
+++ src/main/java/org/example/savecsv/ResultStructure.java
@@ -1,26 +1,28 @@
 package org.example.savecsv;
 
+import org.example.pojos.ResultPojo;
+
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class ResultStructure {
-    public static void saveObjectToCsv(String[][] resultColumns) throws IOException {
+    public static void saveObjectToCsv(ResultPojo resultPojo) throws IOException {
+        String[][] resultColumns = resultPojo.data;
+        String[] headers = resultPojo.headers;
         int lenHeight = resultColumns[0].length;
         int lenWidth = resultColumns.length;
-        String[] headers = {};
 
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("results.csv"));
 
-       /* for(int h = 0; h<lenWidth; h++) {
+        for(int h = 0; h<lenWidth; h++){
             if (h != 0) {
                 bufferedWriter.write(",");
             }
             bufferedWriter.write((headers[h]));
+            System.out.println(headers[h]);
         }
-        bufferedWriter.newLine();*/
+        bufferedWriter.newLine();
 
         for(int i = 0; i<lenHeight; i++){
             for(int j = 0; j<lenWidth; j++){
@@ -29,7 +31,7 @@ public class ResultStructure {
                 }
                 try{
                     bufferedWriter.write((resultColumns[j][i]));
-                } catch (NullPointerException e){
+                } catch (NullPointerException ignored){
                 }
             }
             if (i!=(lenHeight-1)){
